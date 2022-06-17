3f6a4a33e5ef65aa72cb9c32dc1f09c7d4b28975
Herve2iko <ihervedelice@gmail.com>
2022-06-17 07:31:50 +0200

    user services

---
+++ src/main/java/com/example/crud1/User/UserServices.java
@@ -0,0 +1,40 @@
+package com.example.crud1.User;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserServices {
+    private static List<User> UserList = new ArrayList<User>();
+    private static int count = 3;
+    static{
+        UserList.add(new User(1,"ikori",new Date()));
+        UserList.add(new User(2,"ikori",new Date()));
+        UserList.add(new User(3,"ikori",new Date()));
+    }
+
+    public List<User> findAll(){
+        return UserList;
+    }
+
+    public User addUser(User user){
+        if(user.getId()==null){
+            user.setId(++count);
+        }
+        UserList.add(user);
+
+        return user;
+    }
+
+    public User findOne(int id){
+        for(User user:UserList){
+            if(user.getId() == id){
+                return user;
+            }
+        }
+        return null;
+    }
+}
