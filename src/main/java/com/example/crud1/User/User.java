0b557569c42f49fceee8cc0753f3b75c5ffda834
Herve2iko <ihervedelice@gmail.com>
2022-06-17 07:31:14 +0200

    user bean

---
+++ src/main/java/com/example/crud1/User/User.java
@@ -0,0 +1,51 @@
+package com.example.crud1.User;
+
+import java.util.Date;
+
+public class User {
+    private Integer id;
+    private String name;
+    private Date dob;
+    
+    
+    public User() {
+    }
+
+    public User(Integer id, String name, Date dob) {
+        this.id = id;
+        this.name = name;
+        this.dob = dob;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    @Override
+    public String toString() {
+        return "dob=" + dob + ", id=" + id + ", name=" + name + "";
+    }
+
+    
+    
+}
